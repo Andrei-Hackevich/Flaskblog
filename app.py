8a400472aebff9d6489c85317312b68ff4de11cf
Andrei Khatskevich <andrei_hackevich@mail.ru>
2022-11-16 20:07:03 +0300

    Доработал редактирование статей

--- app.py
+++ app.py
@@ -30,15 +30,18 @@ def index():
 def about():
     return render_template('about.html')
 
+
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
 
+
 @app.route('/posts/<int:id>')
 def posts_detail(id):
     article = Article.query.get(id)
     return render_template('posts_detail.html', article=article)
+
 
 @app.route('/posts/<int:id>/delete')
 def post_delete(id):
@@ -51,17 +54,16 @@ def post_delete(id):
     except:
         return "При удалении возникла ошибка"
 
+
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
 def post_update(id):
+    article = Article.query.get(id)
     if request.method == 'POST':
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-
-        article = Article(title=title, intro=intro, text=text)
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
 
         try:
-            db.session.add(article)
             db.session.commit()
             return redirect('/posts')
         except:
@@ -69,6 +71,7 @@ def post_update(id):
     else:
         article = Article.query.get(id)
         return render_template('post_update.html', article=article)
+
 
 @app.route('/create_article', methods=['POST', 'GET'])
 def create_article():
